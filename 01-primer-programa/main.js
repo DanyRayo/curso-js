e63b2526c7ea0c12d444e430052bcab69559ad4f
ing.rayo <danielrayoherrera@protonmail.com>
2026-06-26 18:17:57 -0600

    Initial commit

---
+++ 01-primer-programa/main.js
@@ -0,0 +1,10 @@
+/*
+alert("Hola que tal, bienvenido al curso");
+alert("Estamos aprendiendo JS");
+*/
+
+// Estos mensajes aparecerán por la consola
+console.log("JavaScript es el lenguaje mas demandado");
+console.log("Soy un mensaje enviado desde JS");
+
+console.log(124 + 11);
