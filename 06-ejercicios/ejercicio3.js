5f69db4ede2ce2fc06b4f59bb0c2f6b14cf3b283
Daniel Rayo <sergio.rayo@grupomedios.com>
2026-07-10 17:38:10 -0600

    Se avanza al ejercicio 3

--- 06-ejercicios/ejercicio3.js
+++ 06-ejercicios/ejercicio3.js
@@ -7,3 +7,9 @@ Utiliza variables y operadores para resolverlo.
 
 */
 
+const minutosHora = 60;
+let horas = 7;
+
+let total = minutosHora * horas;
+
+console.warn("7 horas tienen: " + total + " minutos en total");
